2313c7cd348aa63ed9fec3c226ffd763d7f5935f
catmblake <catmblake@gmail.com>
2018-12-11 15:13:49 -0800

    added logic to bamazonCustomer.js. created connection to bamazon db. displayed inventory in the terminal

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,3 +1,23 @@
 var inquirer = require("inquirer");
 var mysql = require("mysql");
 require("console.table");
+
+var connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "password",
+    database: "bamazon_DB"
+  });
+  // connect to the mysql server and sql database
+  connection.connect(function(err) {
+    if (err) throw err;
+    displayInventory();
+  });
+
+  function displayInventory() {
+    connection.query("SELECT * FROM products", function(err, results) {
+      if (err) throw err;
+      console.table(results);
+  });
+}
