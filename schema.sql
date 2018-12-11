001dcd1e2c9d5ecfd79b7347b0e394e815f76d6b
catmblake <catmblake@gmail.com>
2018-12-11 14:56:24 -0800

    added 10 mock items to the products table in bamazon db

--- schema.sql
+++ schema.sql
@@ -13,4 +13,15 @@ CREATE TABLE products (
     PRIMARY KEY (item_id)
 );
 
+INSERT INTO products (product_name, department_name, price, stock_quantity) VALUES ("dog food", "pets", 24.99, 95);
+INSERT INTO products (product_name, department_name, price, stock_quantity) VALUES ("dog treats", "pets", 14.99, 50);
+INSERT INTO products (product_name, department_name, price, stock_quantity) VALUES ("shampoo", "health and beauty", 6.95, 200);
+INSERT INTO products (product_name, department_name, price, stock_quantity) VALUES ("conditioner", "health and beauty", 7.95, 150);
+INSERT INTO products (product_name, department_name, price, stock_quantity) VALUES ("t-shirt", "apparel", 9.95, 1000);
+INSERT INTO products (product_name, department_name, price, stock_quantity) VALUES ("sweater", "apparel", 15.95, 500);
+INSERT INTO products (product_name, department_name, price, stock_quantity) VALUES ("table cloth", "homeware", 12.75, 25);
+INSERT INTO products (product_name, department_name, price, stock_quantity) VALUES ("curtains", "homeware", 27.95, 40);
+INSERT INTO products (product_name, department_name, price, stock_quantity) VALUES ("kettlebell weights", "sports equipment", 49.50, 10);
+INSERT INTO products (product_name, department_name, price, stock_quantity) VALUES ("yoga mat", "sports equipment", 18.95, 30);
+
 SELECT * FROM products;
