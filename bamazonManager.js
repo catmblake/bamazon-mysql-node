d907f8491fac4d030f39acfce4be4a65c036da4a
catmblake <catmblake@gmail.com>
2018-12-12 20:57:50 -0800

    added logic for case: View Products for Sale

--- bamazonManager.js
+++ bamazonManager.js
@@ -3,14 +3,57 @@ var mysql = require("mysql");
 require("console.table");
 
 var connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: "root",
-  password: "password",
-  database: "bamazon_DB"
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "password",
+    database: "bamazon_DB"
 });
 // connect to the mysql server and sql database
 connection.connect(function (err) {
-  if (err) throw err;
-  console.log("Manager View");
+    if (err) throw err;
+    console.log("Manager Dashboard");
+    beginTasks();
 })
+
+function beginTasks() {
+    inquirer.prompt([{
+        name: "task",
+        type: "list",
+        choices: ["View Products for Sale", "View Low Inventory", "Add to Inventory", "Add New Product"],
+        message: "What would you like to do?"
+    }]).then(function (answer) {
+        switch (answer.task) {
+            case "View Products for Sale":
+                displayProductsForSale();
+                break;
+            case "View Low Inventory":
+                //   code
+                break;
+            case "Add to Inventory":
+                //   code
+                break;
+            case "Add New Product":
+                //   code
+                break;
+        }
+    })
+}
+
+function displayProductsForSale() {
+    connection.query("SELECT * FROM products", function (err, response) {
+        if (err) throw err;
+        console.table(response);
+        inquirer.prompt([{
+            name: "newTask",
+            type: "confirm",
+            message: "Would you like to perform another task?"
+        }]).then(function (answer) {
+            if (answer.newTask) {
+                beginTasks();
+            } else {
+                connection.end();
+            }
+        })
+    })
+}
