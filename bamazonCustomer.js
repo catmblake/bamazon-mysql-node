bfe9f89b219f5e06b6ea5437bea480dedd82a93a
catmblake <catmblake@gmail.com>
2018-12-13 21:14:37 -0800

    added pseudocode and formatting

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,6 +1,8 @@
+// requiring all dependencies used in this application
 var inquirer = require("inquirer");
 var mysql = require("mysql");
 require("console.table");
+// declaring connection variable and storing mysql connection
 var connection = mysql.createConnection({
   host: "localhost",
   port: 3306,
@@ -8,12 +10,16 @@ var connection = mysql.createConnection({
   password: "password",
   database: "bamazon_DB"
 });
-// connect to the mysql server and sql database
+// declaring divider variable to use for console display
+var divider = ("\n---------------------------------------------\n\n")
+// connecting to the mysql server and sql database
 connection.connect(function (err) {
   if (err) throw err;
-  console.log("\nWelcome to bamazon, the leading command line retailer.\nThe items listed below are on sale now!\n");
+  console.log(`${divider}Welcome to bamazon, the leading command line retailer.\n\n`)
+  console.log(`The items listed below are on sale now!${divider}`);
   displayAvailableInventory()
 });
+// displaying product list and asking if customer wishes to start shopping
 function displayAvailableInventory() {
   connection.query("SELECT item_id, product_name, price FROM products WHERE stock_quantity > 0", function (err, results) {
     if (err) throw err;
@@ -26,12 +32,13 @@ function displayAvailableInventory() {
       if (answer.shop) {
         getCustomerOrder(results);
       } else {
-        console.log("Thank you for visiting bamazon. Please come back soon!")
+        console.log(`${divider}Thank you for visiting bamazon. Please come back soon!${divider}`)
         connection.end();
       }
     })
   })
 };
+// getting customer order, checking availability & displaying price
 function getCustomerOrder(results) {
   inquirer.prompt([{
     name: "order",
@@ -42,7 +49,7 @@ function getCustomerOrder(results) {
     }
   }]).then(function (answer) {
     var customerOrder = JSON.parse(answer.order);
-    for (var i = 0; i < results.length; i++) {
+     for (var i = 0; i < results.length; i++) {
       if (customerOrder === results[i].item_id) {
         inquirer.prompt([{
           name: "quantity",
@@ -52,15 +59,15 @@ function getCustomerOrder(results) {
             return !isNaN(value);
           }
         }]).then(function (answer) {
-          var orderQuantity = (answer.quantity);
+          var orderQuantity = JSON.parse(answer.quantity);
           connection.query(`SELECT * FROM products WHERE item_id = ${customerOrder}`, function (err, results) {
             if (err) throw err;
             var orderInfo = `${orderQuantity} of item ${results[0].item_id}: ${results[0].product_name}`;
-            console.log(`You ordered ${orderInfo}`);
+            console.log(`${divider}You ordered ${orderInfo}`);
             var remainingStock = results[0].stock_quantity - orderQuantity;
             if (remainingStock >= 0) {
               var orderPrice = (orderQuantity * results[0].price).toFixed(2);
-              console.log(`Your order total is $${orderPrice}`);
+              console.log(`Your order total is $${orderPrice}${divider}`);
               inquirer.prompt([{
                 name: "checkout",
                 type: "list",
@@ -69,40 +76,24 @@ function getCustomerOrder(results) {
               }]).then(function (answer) {
                 if (answer.checkout === "Proceed to Checkout") {
                   updateProductInventory(remainingStock, customerOrder);
-                  console.log("Thank you for shopping at bamazon!");
+                  console.log(`${divider}Thank you for shopping at bamazon!`);
                   connection.end();
                 } else if (answer.checkout === "Cancel Order") {
-                  console.log("We have saved this order in your cart. Please come back to bamazon when you are ready to complete your purchase")
+                  console.log(`${divider}We have saved this order in your cart. Please come back to bamazon when you are ready to complete your purchase${divider}`)
                   connection.end();
                 }
               })
             } else {
-              console.log("We have insufficient stock to process your order at this time.")
-              inquirer.prompt([{
-                name: "continue",
-                type: "list",
-                choices: ["Find Something Else", "Quit Shopping"],
-                message: "What would you like to do?"
-              }]).then(function (answer) {
-                if (answer.continue === "Find Something Else") {
-                  getCustomerOrder();
-                } else {
-                  console.log("Goodbye. Please check back again soon.");
-                  connection.end();
-                }
-              })
+              console.log(`${divider}We have insufficient stock to process your order at this time.${divider}`)
+              continueOrQuit();
             }
           })
         })
-      } else {
-        connection.end();
-        return console.log("We're sorry this product does not exist in bamazon inventory");
-        // call a function to find another product
-      }
+      } //want to add an else sorry this product doesn't exist but don't know how to with the loop
     }
   })
 }
-
+// function to update inventory levels in the db when customer checks out
 function updateProductInventory(remainingStock, customerOrder) {
   connection.query(
     "UPDATE products SET ? WHERE ?",
@@ -114,9 +105,25 @@ function updateProductInventory(remainingStock, customerOrder) {
         item_id: customerOrder
       }
     ],
-    function (err, res) {
+    function (err, result) {
       if (err) throw err;
-      console.log("Your order is complete!");
+      console.log(`Your order is complete!${divider}`);
     }
   );
+}
+// function asking customer if they want to checkout or cancel
+function continueOrQuit () {
+  inquirer.prompt([{
+    name: "continue",
+    type: "list",
+    choices: ["Find Something Else", "Quit Shopping"],
+    message: "What would you like to do?"
+  }]).then(function (answer) {
+    if (answer.continue === "Find Something Else") {
+      getCustomerOrder();
+    } else {
+      console.log(`${divider}Goodbye. Please check back again soon.${divider}`);
+      connection.end();
+    }
+  })
 }
