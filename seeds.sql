69e448a8ace90b0628d3332522aa66d7430ec8f7
catmblake <catmblake@gmail.com>
2018-12-14 22:43:55 -0800

    small edits from testing

--- seeds.sql
+++ seeds.sql
@@ -10,12 +10,12 @@ INSERT INTO products (product_name, department_name, price, stock_quantity) VALU
 INSERT INTO products (product_name, department_name, price, stock_quantity) VALUES ("kettlebell weights", "sports equipment", 49.50, 10);
 INSERT INTO products (product_name, department_name, price, stock_quantity) VALUES ("yoga mat", "sports equipment", 18.95, 30);
 -- populating initial departments
-INSERT INTO departments (department_name, over_head_costs) VALUES ("pets", 100.88);
-INSERT INTO departments (department_name, over_head_costs) VALUES ("kids", 100.75);
-INSERT INTO departments (department_name, over_head_costs) VALUES ("apparel", 100.89);
-INSERT INTO departments (department_name, over_head_costs) VALUES ("homeware", 100.93);
-INSERT INTO departments (department_name, over_head_costs) VALUES ("health and beauty", 100.43);
-INSERT INTO departments (department_name, over_head_costs) VALUES ("sports equipment", 100.73);
+INSERT INTO departments (department_name, over_head_costs) VALUES ("pets", 1020.88);
+INSERT INTO departments (department_name, over_head_costs) VALUES ("kids", 4630.75);
+INSERT INTO departments (department_name, over_head_costs) VALUES ("apparel", 2348.89);
+INSERT INTO departments (department_name, over_head_costs) VALUES ("homeware", 2376.93);
+INSERT INTO departments (department_name, over_head_costs) VALUES ("health and beauty", 3476.43);
+INSERT INTO departments (department_name, over_head_costs) VALUES ("sports equipment", 4533.73);
 -- Query to generate sales by department table
 SELECT departments.department_id, departments.department_name, departments.over_head_costs, 
 COALESCE(SUM(products.product_sales), 0) AS product_sales, 
@@ -23,4 +23,4 @@ COALESCE(SUM(products.product_sales), 0) - departments.over_head_costs AS total_
 FROM departments 
 LEFT JOIN products ON departments.department_name = products.department_name 
 GROUP BY departments.department_id 
-ORDER BY departments.department_name
+ORDER BY departments.department_id
